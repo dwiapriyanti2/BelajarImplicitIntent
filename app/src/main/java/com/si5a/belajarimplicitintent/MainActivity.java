09f3a93e32b1e35b7523adffa916d839bfc7acce
dwiarpiayanti2 <sastikadwiapriyanti2@mhs.mdp.ac.id>
2022-10-12 17:01:57 +0700

    Update 12/10

--- app/src/main/java/com/si5a/belajarimplicitintent/MainActivity.java
+++ app/src/main/java/com/si5a/belajarimplicitintent/MainActivity.java
@@ -2,13 +2,42 @@ package com.si5a.belajarimplicitintent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText etUrl, etlokasi, etTeks;
+    Button btnBukaWebsite, btnBukaLokasi, btnBagikanTeks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etUrl = findViewById(R.id.et_website);
+        etlokasi = findViewById(R.id.et_lokasi);
+        etTeks = findViewById(R.id.et_teks);
+        btnBukaWebsite = findViewById(R.id.btn_website);
+        btnBukaLokasi = findViewById(R.id.btn_lokasi);
+        btnBagikanTeks = findViewById(R.id.btn_teks);
+
+        btnBukaWebsite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String urlWebsite = etUrl.getText(). toString();
+                Uri uriUrlWebsite = Uri.parse(urlWebsite);
+                Intent intentWebsite = new Intent(Intent.ACTION_VIEW, uriUrlWebsite);
+                try {
+                    startActivity(intentWebsite);
+                }catch (Exception e){
+                    etUrl.setError("Url Tidak Sesuai");
+                }
+
+            }
+        });
     }
 }
